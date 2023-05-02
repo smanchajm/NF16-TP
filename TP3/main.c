1f458f78a2ce83d29ceeac25b65587075aef8506
smanchajm <samuel.manchajm@gmail.com>
2023-05-02 15:11:22 +0200

    main V1

--- TP3/main.c
+++ TP3/main.c
@@ -8,7 +8,7 @@ int main()
 
 // Voir pour les strcopy avec le nom des rayons
 // Attention débugger supprimerrayon
-
+/*
     char *nomMagasin = (char*) malloc((MAX*sizeof(char)));
     printf("Quel est le nom du magasin ?");
     scanf("%s", nomMagasin);
@@ -23,6 +23,7 @@ int main()
 
     T_Rayon *selec = magasin->liste_rayons;
 
+    // ATTENTIONS DOUBLONS produits
     while (strcmp(n, "o")==0) {
         float prix = 0;
         int quantite = 0;
@@ -52,20 +53,20 @@ int main()
         int res;
         res = supprimerRayon(magasin, nomRayon);
         if(res == 1){
-            printf("Le rayon est bien supprimé !");
+            printf("Le rayon est bien supprime !");
         }
         free(magasin);
         printf("FINISH");
     }
     // ATTEBTION rajouter des conditions d'existence sur afficher produit
-    afficherRayon(magasin->liste_rayons);
+    afficherRayon(magasin->liste_rayons);*/
 
 
-}
 
+    T_Magasin *magasin = NULL;
 
-    // ============= MENU UTILISATEUR ============= */
-/*    char choix = '0';
+    // ============= MENU UTILISATEUR =============
+    char choix = '0';
     while (choix != '9') {
         printf("\n======================================");
         printf("\n1. Creer un magasin");
@@ -83,17 +84,17 @@ int main()
         choix = getchar();
         viderBuffer(); // supprimer de l'entrée standard le retour charriot et les éventuels caractères supplémentaires tapés par l'utilisateur
 
+
+
         switch (choix) {
             case '1' :
             {
                 char *nomMagasin = (char*) malloc((MAX*sizeof(char)));
                 printf("Quel est le nom du magasin ? ");
                 scanf("%s", nomMagasin);
-                T_Magasin *magasin = creerMagasin(nomMagasin);
+                viderBuffer();
+                magasin = creerMagasin(nomMagasin);
             }
-
-
-
                 break;
 
             case '2' :
@@ -101,19 +102,77 @@ int main()
                 char *nomRayon = (char*) malloc(MAX*sizeof(char));
                 printf("Quel est le nom du rayon ?");
                 scanf("%s", nomRayon);
-                ajouterRayon( )
+                viderBuffer();
+                ajouterRayon(magasin, nomRayon);
             }
-
-
                 break;
 
             case '3' :
+            {
+                float prix = 0;
+                int quantite = 0;
+                char *designation = (char*) malloc(MAX*sizeof(char));
+                char *nomRayon = (char*) malloc(MAX*sizeof(char));
+
+                printf("Quel est le nom du rayon ?");
+                scanf("%s", nomRayon);
+                viderBuffer();
+
+                printf("Quel est le nom du produit ?");
+                scanf("%s", designation);
+                viderBuffer();
+
+
+                printf("Quel est le prix du produit ?");
+                scanf("%f", &prix);
+
+
+                printf("Quel est la quantite ?");
+                scanf("%d", &quantite);
+
+
+                // Recherche du rayon
+                printf("%s\n",designation);
+                T_Rayon *selec = magasin->liste_rayons;
+                printf("%s\n",selec->nom_rayon);
+                while (selec != NULL){
+                    if(strcmp(selec->nom_rayon, nomRayon)== 0) break;
+                    selec = selec->suivant;
+                }
+                printf("%s",selec->nom_rayon);
+                if (selec == NULL){
+                    printf("ERREUR");
+                    break;
+                }
+
+                ajouterProduit(selec, designation, prix, quantite);
+                //free(designation);
+                //free(nomRayon);
+            }
+
                 break;
 
             case '4' :
                 break;
 
             case '5' :
+            {
+                char *nomRayon = malloc(MAX * sizeof(char));
+
+                printf("Quel est le nom du rayon ?");
+                scanf("%s", nomRayon);
+
+                // Recherche du rayon
+                T_Rayon *selec = magasin->liste_rayons;
+                while (strcmp(selec->nom_rayon, nomRayon)!=0){
+                    selec = selec->suivant;
+                }
+                afficherRayon(selec);
+                free(nomRayon);
+
+
+
+            }
                 break;
 
             case '6' :
@@ -139,4 +198,4 @@ int main()
     }
 
     return 0;
-}*/
+}
